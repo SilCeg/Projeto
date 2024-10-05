00e48f6bf5259564dd957521db37d4e2c8684404
Gustavo <gustavocs.contato@gmail.com>
2024-10-04 21:57:49 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import http from 'http'
+
+const PORT = 7777; 
+
+const rotas = {
+    '/': "Rota principal",
+    '/level1': "Teste de rota 1",
+    '/level2': "Teste de rota 2"
+}
+
+const anus = http
+
+anus.createServer ((req,res)=>{
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.end (rotas[req.url])
+})
+
+anus.listen(PORT, () => {
+    console.log('Deu certo!')
+})
