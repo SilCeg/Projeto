e52ff629e204cb18b485884f4ba2c6157195b1d7
Silceg <gustavocs.contato@gmail.com>
2024-11-07 21:54:13 -0300

    07-11

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ const express = require("express");
 const rotas = express();
 const Sequelize = require ("sequelize");
 
-const conexaoBanco = new Sequelize("teste", "root", "", {
+const conexaoBanco = new Sequelize("projeto", "root", "", {
     host: "localhost",
     dialect: "mysql",
 });
 
-  const aluno = conexaoBanco.define("alunos", {
+  const aluno = conexaoBanco.define("aluno", {
     nome: {
       type: Sequelize.STRING,
     },
     idade: {
       type: Sequelize.INTEGER,
     },
+    atividade: {
+      type: Sequelize.STRING,
+    }
   });
 
 aluno.sync({ force: false }); 
@@ -22,10 +25,10 @@ rotas.get("/", function (req, res) {
     res.send("Rota principal");
 });
 
-rotas.get("/salvar/:nome/:idade", async function (req, res) {
-    const { nome, idade } = req.params;
+rotas.get("/salvar/:nome/:idade/:atividade", async function (req, res) {
+    const { nome, idade, atividade } = req.params;
 
-    const novoAluno = await aluno.create({ nome, idade }); //função que espera
+    const novoAluno = await aluno.create({ nome, idade, atividade }); //função que espera
   
     res.json({
       resposta: "Aluno criado com sucesso",
@@ -47,6 +50,22 @@ rotas.get("/deletar/:id", async function (req, res) {
       res.status(404).json({ mensagem: "Aluno não encontrado" });
     }
 });
+
+rotas.get("/editar/:id/:nome/:idade/:atividade", async function (req, res) {
+  const { id, nome, idade, atividade } = req.params;
+  const idNumber = parseInt(id, 10); // Converte o ID para número
+
+    const [updated] = await aluno.update(
+    { nome, idade, atividade },
+    {
+      where: { id: idNumber }, // Usa o ID numérico
+    }
+  );
+
+  res.json({
+    mensagem: "Aluno atualizado com sucesso",
+  });
+}); 
 
 rotas.get("/mostrar", async function (req, res) {
     const alunos = await aluno.findAll(); // Busca todos os registros
